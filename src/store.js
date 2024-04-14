9e674e1c2eea622e664d7b6dd98c55faa50ab5f3
Avinash <avinashchandrabarik@gmail.com>
2024-04-14 16:05:05 +0530

    second Phase

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
-import { applyMiddleware, createStore, compose } from "redux";
+import {
+  // applyMiddleware,
+  createStore,
+  // compose
+} from "redux";
 // import reduxLogger from "redux-logger";
 import reducers from "./Reducers/reducers";
 import { persistStore } from "redux-persist";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const middleware = [reduxLogger];
 
 export const store = createStore(
